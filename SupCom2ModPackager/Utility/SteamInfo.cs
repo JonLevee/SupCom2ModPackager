3946e48871c98e7f540f48d4dd2e5f5cde526af7
Jon LeVee <jonlevee@gmail.com>
2025-04-28 00:36:55 -0700

    fixed steaminfo

--- SupCom2ModPackager/Utility/SteamInfo.cs
+++ SupCom2ModPackager/Utility/SteamInfo.cs
@@ -1,40 +1,88 @@
-﻿using System.IO;
+﻿using Microsoft.Win32;
+using System.IO;
+using ValveKeyValue;
 
 namespace SupCom2ModPackager.Utility
 {
+
+    public class SteamApp
+    {
+        public string AppId { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string InstallDir { get; set; } = string.Empty;
+    }
+    public class SteamApps : Dictionary<string, SteamApp>
+    {
+    }
     public class SteamInfo
     {
-        private readonly List<string> steamFolders = new List<string>();
-        private readonly Dictionary<string, string> folderPaths = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        private readonly SteamApps steamApps = [];
         public string GetRoot(string appName)
         {
-            if (!steamFolders.Any())
+            if (!steamApps.Any())
             {
-                var programFilesFolder = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
-                var steamLibraryFolderFile = Path.Combine(programFilesFolder, @"Steam\steamapps\libraryfolders.vdf");
-                var pathKey = "\"path\"";
-                var paths = File.ReadAllLines(steamLibraryFolderFile)
-                    .Where(s => s.Contains(pathKey))
-                    .Select(s => s.Trim().Substring(pathKey.Length).Trim(' ', '\"', '\t'))
-                    .Select(s => s.Replace(@"\\", @"\"))
-                    .ToList();
-                steamFolders.AddRange(paths);
-            }
-            if (!folderPaths.ContainsKey(appName))
-            {
-                foreach (var steamFolder in steamFolders)
+                var regKey = Registry.CurrentUser.OpenSubKey(@"Software\Valve\Steam");
+                var steamPath = regKey?.GetValue("SteamPath")?.ToString()
+                    ?? Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Steam");
+                var steamLibraryFolderFile = Path.Combine(steamPath, @"steamapps\libraryfolders.vdf");
+                var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+                SteamLibraryFolder[] steamLibraryFolders = Array.Empty<SteamLibraryFolder>();
+                using (var stream = File.OpenRead(steamLibraryFolderFile))
                 {
-                    var path = Path.Combine(steamFolder, @"steamapps\common", appName);
-                    if (Directory.Exists(path))
+                    steamLibraryFolders = kv.Deserialize<SteamLibraryFolder[]>(stream);
+
+                    foreach (var steamLibraryFolder in steamLibraryFolders)
                     {
-                        folderPaths.Add(appName, path);
+                        if (steamLibraryFolder.Path != null)
+                        {
+                            var libraryPath = Path.Combine(steamLibraryFolder.Path, "steamapps");
+                            foreach (var manifest in Directory.GetFiles(libraryPath, "*.acf"))
+                            {
+                                using (var manifestStream = File.OpenRead(manifest))
+                                {
+                                    var app = kv.Deserialize<SteamLibraryApp>(manifestStream);
+                                    if (steamLibraryFolder.Apps.ContainsKey(app.AppId))
+                                    {
+                                        var steamApp = new SteamApp
+                                        {
+                                            AppId = app.AppId,
+                                            Name = app.Name,
+                                            InstallDir = Path.Combine(
+                                                steamLibraryFolder.Path.Replace("\\\\", "\\"),
+                                                "steamapps",
+                                                "common", 
+                                                app.InstallDir)
+                                        };
+                                        steamApps.Add(app.Name, steamApp);
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
 
-            var root = folderPaths[appName];
+            var requestedApp = steamApps[appName];
+            var root = requestedApp.InstallDir;
 
             return root;
+        }
+        private class SteamLibraryFolder
+        {
+            public string Path { get; set; } = string.Empty;
+            public string Label { get; set; } = string.Empty;
+            public long ContentId { get; set; } = 0;
+            public long TotalSize { get; set; } = 0;
+            public Dictionary<string, string> Apps { get; set; } = [];
+        }
+        private class SteamLibraryApp
+        {
+            public string AppId { get; set; } = string.Empty;
+            public string Name { get; set; } = string.Empty;
+            public string InstallDir { get; set; } = string.Empty;
+        }
+        private class SteamLibraryApps : Dictionary<int, SteamLibraryApp>
+        {
         }
     }
 
